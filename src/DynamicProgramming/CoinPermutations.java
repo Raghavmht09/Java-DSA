6377a780957bea0f507d8ecbd40cd5931ad047e7
Raghavmht09 <52198275+Raghavmht09@users.noreply.github.com>
2020-08-08 17:52:55 +0530

    Add files via upload

---
+++ src/DynamicProgramming/CoinPermutations.java
@@ -0,0 +1,33 @@
+package DynamicProgramming;
+import java.util.*;
+public class CoinPermutations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc =new Scanner(System.in);
+int n= sc.nextInt();
+int [] coins = new int [n];
+
+for (int i=0;i<coins.length;i++) {
+	coins[i]=sc.nextInt();
+	
+}
+
+int tar= sc.nextInt();
+int [] dp= new int [tar+1];
+dp[0]=1;
+
+for(int amt =1;amt<=tar;amt++) {
+	for(int coin:coins) {
+		if(coin<=amt) {
+			int ramt=amt-coin;
+			dp[amt]+=dp[ramt];
+		}
+		
+	}
+}
+   System.out.println(dp[tar]);
+
+	}
+
+}
