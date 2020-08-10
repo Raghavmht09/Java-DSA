3bf0334d4b23e3c7a733d2b863647b71332863fe
Raghavmht09 <52198275+Raghavmht09@users.noreply.github.com>
2020-08-10 23:01:14 +0530

    Add files via upload

---
+++ src/DynamicProgramming/MaxSumNonAdjacent.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+import java.util.*;
+
+public class MaxSumNonAdjacent {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+		int arr[]= new int[n];
+
+		for (int i=0;i<n;i++) {
+			arr[i]= sc.nextInt();
+			
+		}
+		int inc= arr[0];
+		int exc=0;
+		for(int i=1;i<n;i++) {
+			int ninc= exc +arr[i];
+			int nexc= Math.max(inc, exc);
+			inc=ninc;
+			exc=nexc;
+		}
+int ans= Math.max(inc, exc);
+System.out.println(ans);
+
+	}
+
+}
