6dd62e080b80284baddd8fb3c47f34b49d9a738f
Raghavmht09 <52198275+Raghavmht09@users.noreply.github.com>
2020-09-03 23:57:01 +0530

    Add files via upload

---
+++ src/interviewQues/PairequalsSum.java
@@ -0,0 +1,50 @@
+package interviewQues;
+import java.util.*;
+class PairequalsSum 
+{ 
+  
+    static boolean pairInSortedRotated(int arr[],  
+                                    int n, int x) 
+    { 
+      
+        int i; 
+        for (i = 0; i < n - 1; i++) 
+            if (arr[i] > arr[i+1]) 
+                break; 
+                  
+        int l = (i + 1) % n; 
+                           
+        int r = i;       
+      
+        while (l != r) 
+        { 
+           
+             if (arr[l] + arr[r] == x) 
+                  return true; 
+       
+      
+             if (arr[l] + arr[r] < x) 
+                  l = (l + 1) % n; 
+                    
+             else  
+                  r = (n + r - 1) % n; 
+        } 
+        return false; 
+    } 
+  
+   
+    public static void main (String[] args) 
+    { 
+        int arr[] = {11, 15, 6, 8, 9, 10}; 
+        int sum = 16; 
+        int n = arr.length; 
+       
+        if (pairInSortedRotated(arr, n, sum)) 
+            System.out.print("Array has two elements" + 
+                             " with sum 16"); 
+        else
+            System.out.print("Array doesn't have two" +  
+                             " elements with sum 16 "); 
+    } 
+} 
+
